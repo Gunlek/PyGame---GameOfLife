aa9c565b3e8cda9ac2d44623a1db4434acf63ded
Fabien Aubret <fabien.aubret@gmail.com>
2017-12-21 13:23:08 +0100

    Finished coding GameOfLife rules

--- GameOfLife.py
+++ GameOfLife.py
@@ -20,11 +20,24 @@ def getCellCoords(actualGrid):
     for l in bufferGrid:
         while l.count(1) > 0:
             OneList += [[lineIndex, l.index(1)]]
+            OneList += [[lineIndex-1, l.index(1)]]
+            OneList += [[lineIndex + 1, l.index(1)]]
+            OneList += [[lineIndex - 1, l.index(1)-1]]
+            OneList += [[lineIndex + 1, l.index(1)-1]]
+            OneList += [[lineIndex - 1, l.index(1)+1]]
+            OneList += [[lineIndex + 1, l.index(1)+1]]
+            OneList += [[lineIndex, l.index(1)-1]]
+            OneList += [[lineIndex, l.index(1)+1]]
             l[l.index(1)] = 0
         lineIndex += 1
-    return OneList
+    returnList = []
+    for cell in OneList:
+        if returnList.count(cell) == 0:
+            returnList+=[cell]
+    return returnList
 
 def countNeighbour(actualGrid, cellCoords):
+    #displayGrid(actualGrid)
     isLeftBorder = True if cellCoords[1] == 0 else False
     isRightBorder = True if cellCoords[1] == len(actualGrid[1])-1 else False
     isTopBorder = True if cellCoords[0] == 0 else False
@@ -43,14 +56,28 @@ def countNeighbour(actualGrid, cellCoords):
             BuffGrid += [l[cellCoords[1]-1:cellCoords[1]+2]]
         subGrid = BuffGrid
         subGrid[1][1] = 0
-        displayGrid(subGrid)
+        #displayGrid(subGrid)
+        neighbourCount = 0
+        for l in subGrid:
+            neighbourCount+=l.count(1)
+        return neighbourCount
 
 def gridUpdater(actualGrid):
+    bufferGrid = copy.deepcopy(actualGrid)
     cellCoords = getCellCoords(actualGrid)
-
-    print(cellCoords)
+    #print(cellCoords)
     for cell in cellCoords:
-        countNeighbour(actualGrid, cell)
+        neighbour = countNeighbour(actualGrid, cell)
+        if actualGrid[cell[0]][cell[1]] == 1:
+            #print(cell)
+            #print(bufferGrid[cell[0]][cell[1]])
+            bufferGrid[cell[0]][cell[1]] = 0 if neighbour < 2 or neighbour > 3 else 1
+            #print(neighbour)
+            #print(bufferGrid[cell[0]][cell[1]])
+            #print(" ")
+        else:
+            bufferGrid[cell[0]][cell[1]] = 1 if neighbour == 3 else 0
+    return bufferGrid
 
 def displayGrid(actualGrid):
     for l in actualGrid:
@@ -69,7 +96,9 @@ gameGrid[5][7] = 1
 
 displayGrid(gameGrid)
 
-print("")
+gameGrid = gridUpdater(gameGrid)
+displayGrid(gameGrid)
 
-gridUpdater(gameGrid)
+gameGrid = gridUpdater(gameGrid)
+displayGrid(gameGrid)
 
